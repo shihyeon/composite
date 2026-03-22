14fb2b831ca10075f9b041e7f58036f1b18825c8
Seungyeop Lee <lsy020206@outlook.kr>
2026-03-23 04:50:52 +0900

    fix neoforge main typo

--- src/main/kotlin/dev/aperso/composite/CompositeNeoForge.kt
+++ src/main/kotlin/dev/aperso/composite/CompositeNeoForge.kt
@@ -18,7 +18,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 @Mod("composite", dist = [Dist.CLIENT])
-class ComposeNeoForge {
+class CompositeNeoForge {
     val logger: Logger = LoggerFactory.getLogger("Composite")
 
     init {
