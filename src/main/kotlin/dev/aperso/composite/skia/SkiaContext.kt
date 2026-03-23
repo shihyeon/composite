6e5717fcca145ccf7677dae2b5ee01c5ce7aec42
Seungyeop Lee <lsy020206@outlook.kr>
2026-03-23 19:29:58 +0900

    fix opengl state management

--- src/main/kotlin/dev/aperso/composite/skia/SkiaContext.kt
+++ src/main/kotlin/dev/aperso/composite/skia/SkiaContext.kt
@@ -1,19 +1,37 @@
 package dev.aperso.composite.skia
 
-import com.mojang.blaze3d.systems.RenderSystem
 import org.jetbrains.skia.DirectContext
+import org.lwjgl.opengl.WGL
 
 object SkiaContext {
+    private var wglContext: Long = 0
+
+    fun initialize() {
+        if (wglContext != 0L) {
+            println("INFO [SkiaContext]: wglContext is already initialized. Skipping initialization.")
+            return
+        }
+        wglContext = WGL.wglCreateContext(null, WGL.wglGetCurrentDC())
+        if (wglContext == 0L) {
+            return
+        }
+        val currentContext = WGL.wglGetCurrentContext(null)
+        WGL.wglShareLists(null, currentContext, wglContext)
+    }
+
     val directContext: DirectContext by lazy {
         DirectContext.makeGL()
     }
 
-    fun initialize() {
-        directContext
-    }
-
     fun run(runnable: Runnable) {
-        RenderSystem.assertOnRenderThread()
-        runnable.run()
+        val oldContext = WGL.wglGetCurrentContext(null)
+        val dc = WGL.wglGetCurrentDC()
+
+        WGL.wglMakeCurrent(null, dc, wglContext)
+        try {
+            runnable.run()
+        } finally {
+            WGL.wglMakeCurrent(null, dc, oldContext)
+        }
     }
 }
